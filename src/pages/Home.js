0150901222412ac4d520e9360645625a7f4d1a66
Mohammad Malik <76458022+mmalik36@users.noreply.github.com>
2022-12-05 13:55:25 -0500

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "../styles/Home.css";
-import Navbar from "../component/Navbar";
 import logo from "../assets/sushi.png";
 
 function Home() {
   return (
     <React.Fragment>
-      <Navbar />
       <div className="home-body-text">
         <p className="wordBubble">
           We try to show you all the variety of Japanese cuisine and its current
@@ -17,11 +15,7 @@ function Home() {
 
       <img src={logo} className="sushi-logo" alt="sushi"></img>
      
-        <div>
-            <button onClick={event=>window.location.href='./menu'} className="order-btn" >
-                Order Now
-            </button>
-        </div>
+
 
     </React.Fragment>
   );
